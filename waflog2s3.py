d461dd83f861be0e2b4c965c1d8d4182f9093b3c
SCLin <gesarlin0803@gmail.com>
2018-02-26 21:03:37 +0800

    modified:   waflog2s3.py

--- waflog2s3.py
+++ waflog2s3.py
@@ -48,27 +48,33 @@ class CplxDatetimeEncode(json.JSONEncoder):
 
 def lambda_handler(event, context):
 	print('Loading function')
-	#create client to connect AWS service
-	#waf_regional = boto3.client('waf-regional')
+	# create client to connect AWS service
+	# waf_regional = boto3.client('waf-regional')
 	waf = boto3.client('waf')
 	s3 = boto3.client('s3')
-	#get several variables for 'get_sampled_requests()' from environment variables
+	
+	# get WAF acl and rule variables for 'get_sampled_requests()' from event body
 	aclid = event['webaclid']
 	ruleids = event['ruleids']
-
+	
+	# get serveral variables for 'get_sampled_requests()' from os env
 	item_num = int(os.environ['item_num'])
 	interval_time = int(os.environ['interval_time'])
-	#using datetime to create the start&end time for 'get_sampled_requests()'
-	#if necessary you can modify timedelta() to get time window size u want
+	bucket_name =  os.environ['bucket_name']
+	
+	# using datetime to create the start&end time for 'waf.get_sampled_requests()'
+	# if necessary you can modify timedelta() to get time window size u want
 	end_time = datetime.now()
 	start_time = end_time - timedelta(minutes=interval_time)
 	
-	#print start & end time for check timezone is OK
-	#if timezone is wrong , modify the datetime.now() to datetime.utcnow() or datetime.now([tz])
-	#print(start_time,end_time)
+	# print start & end time for check timezone is OK
+	# if timezone is wrong , modify the datetime.now() to datetime.utcnow() or datetime.now([tz]).
+	# print(start_time,end_time)
+	
+	# get each rule id from WebACL
 	while(len(ruleids) > 0):
 		ruleid = ruleids.pop()
-		#get sample log from aws waf
+		# get sample log from aws waf
 		waf_response = waf.get_sampled_requests(
 		    WebAclId=aclid,
 		    RuleId=ruleid,
@@ -79,27 +85,24 @@ def lambda_handler(event, context):
 		    MaxItems= item_num
 		)
 	
-		#print waf_response for log at cw logs and troubleshooting
-		print(waf_response)
+		# print waf_response for log at cw logs and troubleshooting
+		# print(waf_response)
+		# transform format from dict to json
 		waf_sample_logs = json.dumps(waf_response['SampledRequests'],cls=CplxDatetimeEncode)
 		print(waf_sample_logs)
 		
-		#create s3 path
+		# create s3 path
 		path =  ("/".join([aclid,ruleid,str(start_time)])) 
 	
-		#get s3 bucket name from environment variables
-		bucket_name =  os.environ['bucket_name']
-	
-		#change waf_response type from 'dict' type to 'string' type then encode to 'bytes' type
-		#byte_waf_response = str(waf_response).encode()
+		# change waf_response type from 'dict' type to 'string' type then encode to 'bytes' type
 		byte_waf_logs = str(waf_sample_logs).encode()
 		
-		#put waf response to s3
+		# put waf response to s3 
 		s3_response = s3.put_object(
 			Body=byte_waf_logs,
 			Bucket=bucket_name,
 			Key=path
 		)
 		
-		#print s3_response for log at cw logs and troubleshooting
+		# print s3_response for log at cw logs and troubleshooting
 		print(s3_response)
